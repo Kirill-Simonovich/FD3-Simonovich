675cac072ee76f4be311b64fd3bd9262eea28e58
Kirill Simonovich <kiril.simonovich@gmail.com>
2023-04-28 15:18:56 +0300

    homework-1

---
+++ Homework-1/Products.js
@@ -0,0 +1,34 @@
+﻿var Products = React.createClass({
+
+  displayName: 'Products',
+
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+    productRow: React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          name: React.PropTypes.string.isRequired,
+          url: React.PropTypes.string.isRequired,
+          price: React.PropTypes.number.isRequired,
+          stock: React.PropTypes.number.isRequired
+        })
+    )
+  },
+
+  render: function () {
+
+    var productRows = this.props.productRow.map(v =>
+        React.DOM.table({ key: v.name, className: 'Product ' },
+            React.DOM.td({ className: 'Cell' }, v.name),
+            React.DOM.td({ className: 'Cell' }, v.price + ' $'),
+            React.DOM.td({ className: 'Cell' }, v.stock + ' шт.'),
+            React.DOM.img({ className: 'Photo', src: v.url })
+        )
+    );
+    return React.DOM.div({ className: 'Product' },
+        React.DOM.div({ className: 'Heading' }, this.props.title),
+        React.createElement(TitleRow),
+        React.DOM.tr({ className: 'Products' }, productRows)
+    );
+  }
+
+});
---
+++ Homework-1/TitleRow.js
@@ -0,0 +1,14 @@
+﻿var TitleRow = React.createClass({
+
+  displayName: 'TitleRow',
+
+  render: function () {
+    return React.DOM.table({ className: 'Product ' },
+        React.DOM.td({ className: 'TitleRow' }, 'Название'),
+        React.DOM.td({ className: 'TitleRow' }, 'Цена'),
+        React.DOM.td({ className: 'TitleRow' }, 'В наличии'),
+        React.DOM.td({ className: 'TitleRow' }, 'Фото')
+    );
+  }
+
+});
